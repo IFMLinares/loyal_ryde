c97b03e571456dd8fd12807fa544fedf10fef4f9
Miguel Linares <ifmlinares@gmail.com>
2024-07-22 16:18:29 -0400

    Changes

--- apps/loyalRyde/models.py
+++ apps/loyalRyde/models.py
@@ -285,7 +285,7 @@ class TransferRequest(models.Model):
 
     def enviar_id_a_usuario(self):
         CustomUser = get_user_model()
-        conductor = CustomUser.objects.get(pk=self.user_driver)
+        conductor = CustomUser.objects.get(pk=self.user_driver.user.pk)
         if conductor:
             serialized_transfer = serialize("json", [self], use_natural_foreign_keys=True)
             rates = {
