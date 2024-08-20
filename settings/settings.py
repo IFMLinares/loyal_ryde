4b947095acfabff59e7da604e9261efcd9735ea0
Miguel Linares <ifmlinares@gmail.com>
2024-08-20 19:14:22 -0400

    Changes

--- settings/settings.py
+++ settings/settings.py
@@ -207,7 +207,7 @@ CHANNEL_LAYERS = {
 	'default': {
 		'BACKEND': 'channels_redis.core.RedisChannelLayer',
 		'CONFIG': {
-			'hosts': [('127.0.0.1', 6379)]
+			'hosts': [('185.182.186.224', 6379)]
 		}
 	}
 }
