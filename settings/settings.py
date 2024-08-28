469c99115ac032fbc5af45bf64d362233ebc5587
Miguel Linares <ifmlinares@gmail.com>
2024-08-28 01:17:23 -0400

    cambios

--- settings/settings.py
+++ settings/settings.py
@@ -163,8 +163,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER =  'loyalride.test@gmail.com'
-EMAIL_HOST_PASSWORD = 'tiyz iguw zekw ygsx'
+EMAIL_HOST_USER =  os.environ.get('DJANGO_EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
