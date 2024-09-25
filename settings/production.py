9b1c3e5edd44e1d0d3f68858e5a58e45b168ae18
Miguel Linares <ifmlinares@gmail.com>
2024-09-25 17:32:37 -0400

    Changes

--- settings/production.py
+++ settings/production.py
@@ -1,3 +1,5 @@
 from .settings import *
-ALLOWED_HOSTS = ['miloyalride.com', 'www.miloyalride.com', 'https://miloyalride.com', '69.167.167.154']
-DEBUG = True
+
+ALLOWED_HOSTS = ['miloyalride.com', 'www.miloyalride.com', '69.167.167.154']
+CSRF_TRUSTED_ORIGINS = ['https://miloyalride.com', 'https://www.miloyalride.com', 'https://69.167.167.154']
+DEBUG = False
