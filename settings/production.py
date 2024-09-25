c05eed8a66534f5886fe51055593eb2feb4e0465
Miguel Linares <ifmlinares@gmail.com>
2024-09-25 17:25:47 -0400

    CHanges

--- settings/production.py
+++ settings/production.py
@@ -1,3 +1,3 @@
 from .settings import *
-ALLOWED_HOSTS = ['miloyalride.com', 'www.miloyalride.com', '69.167.167.154']
+ALLOWED_HOSTS = ['miloyalride.com', 'www.miloyalride.com', 'https://miloyalride.com', '69.167.167.154']
 DEBUG = True
