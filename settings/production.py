b050b1401d7abbb20479a1223e02449036ea1d65
Miguel Linares <ifmlinares@gmail.com>
2026-01-16 17:37:53 -0400

    fix: Add 'https://loyalride.top/' to CSRF_TRUSTED_ORIGINS for enhanced security

--- settings/production.py
+++ settings/production.py
@@ -1,5 +1,6 @@
 from .settings import *
 
 ALLOWED_HOSTS = ['miloyalride.com', 'www.miloyalride.com', '69.167.167.154']
-CSRF_TRUSTED_ORIGINS = ['https://miloyalride.com', 'https://www.miloyalride.com', 'https://69.167.167.154', 'http://miloyalride.com', 'http://www.miloyalride.com', 'http://69.167.167.154']
+CSRF_TRUSTED_ORIGINS = ['https://miloyalride.com', 'https://www.miloyalride.com', 'https://69.167.167.154', 'http://miloyalride.com', 'http://www.miloyalride.com', 'http://69.167.167.154', 
+    'https://loyalride.top/',]
 DEBUG = False
