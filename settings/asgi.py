419dac151667e31d84242382a09532fea497bc8f
Miguel Linares <ifmlinares@gmail.com>
2026-01-17 00:35:13 -0400

    refactor: Reorganize imports in asgi.py for improved readability

--- settings/asgi.py
+++ settings/asgi.py
@@ -1,30 +1,30 @@
 """
 ASGI config for settings project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
 
-from apps.api_app.routing import websocket_urlpatterns
+# 1. Configurar la variable de entorno PRIMERO
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
+
+# 2. Inicializar Django AHORA (Esto carga apps y modelos)
+django_asgi_app = get_asgi_application()
+
+# -----------------------------------------------------------
+# 3. IMPORTAR EL RESTO DESPUÉS DE INICIALIZAR DJANGO
+# (Si importas esto antes, da error porque Django no está listo)
+# -----------------------------------------------------------
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
-
-django_asgi_app = get_asgi_application()
+from apps.api_app.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-	'http': django_asgi_app,
-	'websocket': AllowedHostsOriginValidator(
-		JWTAuthMiddlewareStack(
-			URLRouter(websocket_urlpatterns)
-		)
-	)
+    'http': django_asgi_app,
+    'websocket': AllowedHostsOriginValidator(
+        JWTAuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
+    )
 })
